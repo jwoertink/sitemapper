35b75a1cdf30db889c918261d326d80a96f41b54
Jeremy Woertink <jeremywoertink@gmail.com>
2018-05-22 08:39:26 -0700

    don't need to force extra typing. If someone puts a max of a negative number, then that's their fault when it blows up

--- src/sitemapper/config.cr
+++ src/sitemapper/config.cr
@@ -2,12 +2,12 @@ module Sitemapper
   class Config
     property use_index : Bool
     property host : String
-    property max_urls : UInt32
+    property max_urls : Int32
 
     def initialize
       @use_index = false
       @host = "http://example.com"
-      @max_urls = 500_u32
+      @max_urls = 500
     end
   end
 end
