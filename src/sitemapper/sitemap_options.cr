09b6d2dc3cb3d49159afc9ed3a7afa3a60b86e0a
Anton Maminov <anton.linux@gmail.com>
2020-02-22 12:18:35 +0200

    update for crystal 0.33.0 support

--- src/sitemapper/sitemap_options.cr
+++ src/sitemapper/sitemap_options.cr
@@ -1,6 +1,6 @@
 record SitemapOptions,
   changefreq = "daily",
   priority = 0.5,
-  lastmod : Time = Time.now,
+  lastmod : Time = Time.utc,
   video : Sitemapper::VideoMap? = nil,
   image : Sitemapper::ImageMap? = nil
