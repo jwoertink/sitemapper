193661a37d6daac5cced1ef80ffb13dc6afbfeb6
Stephen Dolan <dolan.stephen1@gmail.com>
2021-06-01 16:53:00 -0400

    Add namespace attributes for images and videos (#20)
    * Add namespace attributes for images and videos
    
    I kept running into errors like this when checking my sitemap that's chock full of videos:
    
    > Namespace prefix video on thumbnail_loc is not defined
    
    I checked Google's docs, and for both Images and Videos they say that we need to include these namespaces:
    - https://developers.google.com/search/docs/advanced/sitemaps/video-sitemaps#xml-namespace
    - https://developers.google.com/search/docs/advanced/sitemaps/image-sitemaps#xml-namespace
    
    I'm sure there's something "smarter" that could be done to only add these when videos/images are present, but I don't think that the extra characters will be of much consequence.
    
    I also wasn't sure if Bing has their own namespace or something, but I couldn't find anything on their documentation so I'm sort of assuming that the whole internet is letting Google define the namespaces.
    
    * See if workflows will run
    
    * Small tweak back to original to prompt workflows?
    
    * Updates for Crystal format
    
    * Fix specs with updated namespace inclusion

--- spec/sitemapper_builder_spec.cr
+++ spec/sitemapper_builder_spec.cr
@@ -44,7 +44,7 @@ describe Sitemapper::Builder do
       XML
 
       xml.should contain <<-XML
-      <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+      <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
       XML
 
       xml.should contain <<-XML
