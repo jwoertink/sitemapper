2d5551eb7524948f05f5e2dc32d65e566a23d9df
Santiago Behak <santiago.behakarburuas@therealreal.com>
2026-02-12 13:41:57 -0300

    [GROWTH-1304] fix memory issue when sitemap files are too long

--- src/sitemapper/builder/stream_builder.cr
+++ src/sitemapper/builder/stream_builder.cr
@@ -12,47 +12,49 @@ module Sitemapper
       @filenames = [] of String
       @index_filenames = [] of String
       @sitemaps = [] of Hash(String, String)
+      @current_page = 1
     end
 
     def add(path, **kwargs) : self
       options = SitemapOptions.new(**kwargs)
       paginator.add(path, options)
+      if paginator.paths.size.modulo(@max_urls).zero?
+        flush
+      end
       self
     end
 
     def index_add(path) : self
-      paginator.index_add(path)
+      @index_filenames << path
       self
     end
 
-    def flush(page)
-      filename = filename_for_page(page)
+    def flush
+      filename = filename_for_current_page
       doc = build_xml_for_page(paginator.items(page))
       @filenames << filename
 
       storage = @storage.new([{"name" => filename, "data" => doc}])
       storage.save(@storage_path)
+      
+      @current_page += 1
+      @paginator = Paginator.new(limit: @max_urls)
     end
 
     def finish : Void
-      paginator.total_pages.times do |page|
-        flush(page + 1)
-      end
-
       if @use_index
         save_index
       end
     end
 
     private def save_index : Void
-      @index_filenames += paginator.index_items
       index = generate_index(@index_filenames + @filenames)
       storage = @storage.new([index])
       storage.save(@storage_path)
     end
 
-    private def filename_for_page(page)
-      Sitemapper.config.sitemap_file_name + "#{page}.xml"
+    private def filename_for_current_page
+      Sitemapper.config.sitemap_file_name + "#{@current_page}.xml"
     end
   end
 end
